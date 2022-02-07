832aad5e968cb1ef75ce9c085d96334ea7f49bfc
Charles Spencer Blancas <charles.blancas@mail.mcgill.ca>
2022-02-06 22:23:47 -0500

    Completed exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = (array, ...toRemove) => {
+    for (let element of toRemove) {
+        let index = array.indexOf(element);
+        if (index == -1) continue;
+        else {
+            array.splice(index, 1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
