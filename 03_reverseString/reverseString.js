49bf24f09c1a60578adaa2d82fc00a3c5de031dd
Charles Spencer Blancas <charles.blancas@mail.mcgill.ca>
2022-02-06 22:09:22 -0500

    Completed exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = (string) => {
+    let length = string.length;
+    output = '';
+    for (let i = length - 1; i >= 0; i--) {
+        output = output + string[i];
+    }
+    return output;
 };
 
 // Do not edit below this line
