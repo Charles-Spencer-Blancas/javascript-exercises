430c336c21f954ce61a7755da308b5b4bcb9abd6
Charles Spencer Blancas <charles.blancas@mail.mcgill.ca>
2022-02-07 15:20:58 -0500

    Completed exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = (min, max) => {
+    if (!Number.isInteger(min) || !Number.isInteger(max)) return 'ERROR';
+    if (min < 0 || max < 0) return 'ERROR';
+    if (min > max) {
+        tmp = max;
+        max = min;
+        min = tmp;
+    }
 
+    let range = max - min;
+    let add = max + min;
+    if (range % 2 == 0) {
+        return add * parseInt(range / 2) + parseInt(add / 2);
+    } else {
+        return add * parseInt((range + 1) / 2);
+    }
 };
 
 // Do not edit below this line
