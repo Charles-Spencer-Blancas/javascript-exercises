02e444200c1c69c250c029a25a6e513b76196671
Charles Spencer Blancas <charles.blancas@mail.mcgill.ca>
2022-02-06 22:00:32 -0500

    Completed exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = (word, repeatTimes) => {
+    if (repeatTimes < 0) return 'ERROR';
+    let output = '';
+    for (let i = 0; i < repeatTimes; i++) {
+        output = output + word;
+    }
+    return output;
 };
 
 // Do not edit below this line
